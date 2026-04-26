8249817b73a74157c9ce1da59bebedb1506e947c
ngeorgina06 <noemiegeorgina123@gmail.com>
2026-04-26 18:02:43 -0400

    Ajout du javascript pour la modale

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const buttons = document.querySelectorAll('.btn-plus');
+
+const modal = document.querySelector(".modal");
+
+for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+
+    button.addEventListener("click", openModal);
+}
+
+function openModal() {
+    const path = this.previousElementSibling.src;
+    const text = document.createElement("textContent");
+    text.textContent = "modal";
+    text.classList.add('zoom');
+
+    modal.appendChild(text);
+    modal.classList.add('visible');
+
+    /* Fermeture modale */
+    text.addEventListener("click", closeModal);
+}
+
+function closeModal(event) {
+    modal.classList.remove("visible");
+    modal.removeChild(event.currentTarget);
+}
